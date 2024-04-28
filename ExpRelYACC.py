97571506859bebadbbef36034e835f953889dc77
a97455 <a97455@alunos.uminho.pt>
2024-04-28 15:43:37 +0100

    funcoes terminadas

--- ExpRelYACC.py
+++ ExpRelYACC.py
@@ -43,7 +43,16 @@ def p_Decl(p):
         parser.nextAdr += 1
         p[0] = ''
 
-def p_Atrib(p):
+def p_Atrib1(p):
+    """Atrib : Fatores VAR '!'"""
+    if p[2] in parser.tabVAR.keys():
+        p[0] = p[1]+'storeg '+str(parser.tabVAR[p[2]])+'\n'
+    else:
+        print(f"Erro semântico: Variável {p[2]} não declarada.")
+        parser.success = False
+        p[0] = ''
+
+def p_Atrib2(p):
     """Atrib : Exp VAR '!'"""
     if p[2] in parser.tabVAR.keys():
         p[0] = p[1]+'storeg '+str(parser.tabVAR[p[2]])+'\n'
@@ -91,64 +100,72 @@ def p_FuncCont7(p):
     p[0] = p[1] + ['mod\n']
 
 def p_FuncAtrib(p):
-    """FuncAtrib : Exp Exp VAR"""
-    if p[3] in parser.tabFunc.keys():
-        p[0]=p[1]+p[2]
-        for cont in parser.tabFunc[p[3]][1]:
+    """FuncAtrib : Fatores VAR"""
+    if p[2] in parser.tabFunc.keys():
+        p[0]=p[1]
+        for cont in parser.tabFunc[p[2]][1]:
             p[0]+=cont
-        p[0]+='storeg '+str(parser.tabFunc[p[3]][0])+'\n'
+        p[0]+='storeg '+str(parser.tabFunc[p[2]][0])+'\n'
     else:
-        print(f"Erro semântico: Função {p[3]} não declarada.")
+        print(f"Erro semântico: Função {p[2]} não declarada.")
         parser.success = False
         p[0] = ''
 
 def p_Exp1(p):
-    """Exp : Fator"""
+    """Exp : Fatores"""
     p[0] = p[1]
 
 def p_Exp2(p): 
-    """Exp : Exp Exp '+'"""
-    p[0] = p[1]+p[2]+'add\n'
+    """Exp : Exp '+'"""
+    p[0] = p[1]+'add\n'
 
 def p_Exp3(p): 
-    """Exp : Exp Exp '-'"""
-    p[0] = p[1]+p[2]+'sub\n'
+    """Exp : Exp '-'"""
+    p[0] = p[1]+'sub\n'
 
 def p_Exp4(p): 
-    """Exp : Exp Exp '*'"""
-    p[0] = p[1]+p[2]+'mul\n'
+    """Exp : Exp '*'"""
+    p[0] = p[1]+'mul\n'
 
 def p_Exp5(p): 
-    """Exp : Exp Exp '/'"""
-    p[0] = p[1]+p[2]+'div\n'
+    """Exp : Exp '/'"""
+    p[0] = p[1]+'div\n'
 
 def p_Exp6(p): 
-    """Exp : Exp Exp MOD"""
-    p[0] = p[1]+p[2]+'mod\n'
+    """Exp : Exp MOD"""
+    p[0] = p[1]+'mod\n'
 
 def p_Exp7(p): 
-    """Exp : Exp Exp GREATEREQUAL"""
-    p[0] = p[1]+p[2]+'supeq\n'
+    """Exp : Exp GREATEREQUAL"""
+    p[0] = p[1]+'supeq\n'
 
 def p_Exp8(p): 
-    """Exp : Exp Exp GREATER"""
-    p[0] = p[1]+p[2]+'sup\n'
+    """Exp : Exp GREATER"""
+    p[0] = p[1]+'sup\n'
 
 def p_Exp9(p): 
-    """Exp : Exp Exp LESSEQUAL"""
-    p[0] = p[1]+p[2]+'infeq\n'
+    """Exp : Exp LESSEQUAL"""
+    p[0] = p[1]+'infeq\n'
 
 def p_Exp10(p): 
-    """Exp : Exp Exp LESS"""
-    p[0] = p[1]+p[2]+'inf\n'
+    """Exp : Exp LESS"""
+    p[0] = p[1]+'inf\n'
 
 def p_Exp11(p): 
-    """Exp : Exp Exp EQUAL"""
-    p[0] = p[1]+p[2]+'equal\n'
+    """Exp : Exp EQUAL"""
+    p[0] = p[1]+'equal\n'
 
 def p_Exp12(p): 
-    """Exp : Exp Exp NOTEQUAL"""
-    p[0] = p[1]+p[2]+'equal\n'+'not\n'
+    """Exp : Exp NOTEQUAL"""
+    p[0] = p[1]+'equal\n'+'not\n'
+
+def p_Fatores1(p):
+    """Fatores : Fator"""
+    p[0]=p[1]
+
+def p_Fatores2(p):
+    """Fatores : Fatores Fator"""
+    p[0]=p[1]+p[2]
 
 def p_Fator(p):
     """Fator : NUM"""
